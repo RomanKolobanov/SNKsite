90f7fba1a0fe577c1fbf165a824bd67c8d8cf90b
RomanKolobanov <54135488+RomanKolobanov@users.noreply.github.com>
2019-09-04 18:36:25 +0300

    Add page marketplace and minification files.

---
+++ page/marketplace.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>SNK - Сайты</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans:300,400,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="/content/ico-stroke/Pe-icon-7-stroke.css">
+	<link rel="stylesheet" href="/content/social-ico/flaticon.css">
+	<link rel="stylesheet" href="/css/navigation-block.css">
+	<link rel="stylesheet" href="/css/stylesheet.css">
+	<link rel="stylesheet" href="/css/animate.css">
+	<link rel="stylesheet" href="css/marketplace-page.css">
+</head>
+<body>
+	<div id="wrap" class="wrap">
+		<div id="nav-wrap" class="nav_wrap">
+        	<div class="menu_nav">
+      			<img src="/content/nav/logo.png" alt="">
+      			<ul class="menu">
+          			<li class="inner-list"><a href="#"><i class="pe-7s-angle-right pe-2x"></i>О нас</a></li>
+          			<li style="" class="lili inner-list">
+            			<a href="#"><i class="pe-7s-angle-right pe-2x"></i>Сайты</a>
+           					 <ul class="submenu">
+             					<li class="animate-list"><a href="">Lorem</a></li>
+              					<li class="animate-list"><a href="">Lorem</a></li>
+           					</ul>
+          			</li>
+          			<li class="lili inner-list">
+           		 	<a href="#"><i class="pe-7s-angle-right pe-2x"></i>Реклама</a>
+            			<ul class="submenu">
+            				<li class="animate-list"><a href="">Lorem</a></li>
+              				<li class="animate-list"><a href="">Lorem</a></li>
+            			</ul>
+          			</li>
+        		</ul>
+        		<div class="social-inner">
+         			<a href="#"><i class="flaticon-instagram-logo"></i></a>
+          			<a href="#"><i class="flaticon-facebook-logo-button"></i></a>
+          			<a href="#"><i class="flaticon-twitter-logo-button"></i></a>
+        		</div>
+        		<div class="fix">
+         			<p class="hover-none">Все права защищены. Любое копирование запрещено</p>
+        		</div>
+      		</div>
+    	</div>
+	</div>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="lib/js/fix-nav.js"></script>
+<script src="/lib/js/animation-nav.js"></script>
+<script src="/lib/js/nav-menu.js"></script>
+<script src="/lib/js/viewportchecker.js"></script>
+</body>
+</html>
